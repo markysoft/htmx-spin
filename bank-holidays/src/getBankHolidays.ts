2c9a3e452baa38f7f255eecf105f69569e817287
markysoft <markharrop@yahoo.com>
2025-01-19 13:48:18 +0000

    Add linting

--- bank-holidays/src/getBankHolidays.ts
+++ bank-holidays/src/getBankHolidays.ts
@@ -1,21 +1,21 @@
-import { z } from 'zod';
-import { getOneYearsTime } from './lib/dateUtils';
-import { BankHoliday, BankHolidaySchema } from './schemas/schema';
+import { z } from 'zod'
+import { getOneYearsTime } from './lib/dateUtils'
+import { BankHoliday, BankHolidaySchema } from './schemas/schema'
 
 export async function getBankHolidays() {
-    
-    const holidaysResponse = await fetch("https://www.gov.uk/bank-holidays.json");
+
+    const holidaysResponse = await fetch("https://www.gov.uk/bank-holidays.json")
     if (!holidaysResponse.ok) {
-        throw new Error(`Failed to fetch bank holidays: ${holidaysResponse.statusText}`);
+        throw new Error(`Failed to fetch bank holidays: ${holidaysResponse.statusText}`)
     }
     const {
         'england-and-wales': { events },
 
-    } = await holidaysResponse.json();
+    } = await holidaysResponse.json()
 
-    const now = new Date();
+    const now = new Date()
     const filter = (e: BankHoliday) => e.date > now && e.date < getOneYearsTime(now)
 
-    var holidays = z.array(BankHolidaySchema).parse(events).filter(filter);
-    return holidays;
+    const holidays = z.array(BankHolidaySchema).parse(events).filter(filter)
+    return holidays
 }
--- bank-holidays/src/index.ts
+++ bank-holidays/src/index.ts
@@ -1,28 +1,28 @@
 
-import { ResponseBuilder } from "@fermyon/spin-sdk";
+import { ResponseBuilder } from "@fermyon/spin-sdk"
 import * as Sqrl from 'squirrelly'
-import { handleError } from "./lib/handleError";
-import { getBankHolidays } from './getBankHolidays';
-import { holidayTemplate } from "./templates/holiday-list";
+import { handleError } from "./lib/handleError"
+import { getBankHolidays } from './getBankHolidays'
+import { holidayTemplate } from "./templates/holiday-list"
 
 export async function handler(req: Request, res: ResponseBuilder) {
 
     try {
-        const contentType = req.headers.get("Content-Type");
-        console.log("Content-Type:", contentType);
+        const contentType = req.headers.get("Content-Type")
+        console.log("Content-Type:", contentType)
 
-        const holidays = await getBankHolidays();
+        const holidays = await getBankHolidays()
         console.log(holidays[0])
         if (contentType === "application/json") {
-            res.set("Content-Type", "application/json");
-            res.send(JSON.stringify(holidays));
+            res.set("Content-Type", "application/json")
+            res.send(JSON.stringify(holidays))
         } else {
-            res.set("Content-Type", "text/plain");
+            res.set("Content-Type", "text/plain")
 
-            res.send(Sqrl.render(holidayTemplate, { holidays }));
+            res.send(Sqrl.render(holidayTemplate, { holidays }))
         }
     }
     catch (error: unknown) {
-        handleError(res, error);
+        handleError(res, error)
     }
 }
