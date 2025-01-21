1d04a91791f260f9acbfaaf61508efe11c816009
markysoft <markharrop@yahoo.com>
2025-01-21 18:24:09 +0000

    restructure

--- hateoas-api/src/index.ts
+++ hateoas-api/src/index.ts
@@ -1,15 +1,11 @@
 
 import { ResponseBuilder, Router } from '@fermyon/spin-sdk'
-import * as Sqrl from 'squirrelly'
 import { handleError } from './lib/handleError'
-import { getBankHolidays } from './getBankHolidays'
-import { holidayTemplate } from './templates/holiday-list'
-import { getTides } from './lib/getTides'
-import { tideTemplate } from './templates/tides'
-
+import { handleDefaultRoute } from './routes/handleDefaultRoute'
+import { handleGetBankHolidays } from './routes/handleGetBankHolidays'
+import { handleGetTides } from './routes/handleGetTides'
 
 const router = Router()
-
 
 router.get(
     '/api/bank-holidays',
@@ -22,46 +18,12 @@ router.get(
 
 router.all('*', (_, req, res) => { handleDefaultRoute(req, res) })
 
-async function handleGetBankHolidays(req: Request, res: ResponseBuilder) {
-    console.log('getting bank holidays')
-    const holidays = await getBankHolidays()
-    if (isJsonRequest(req)) {
-        res.set('Content-Type', 'application/json')
-        res.send(JSON.stringify(holidays))
-    } else {
-        res.set('Content-Type', 'text/plain')
-        res.send(Sqrl.render(holidayTemplate, { holidays }))
-    }
-}
-
-async function handleGetTides(req: Request, res: ResponseBuilder) {
-    console.log('getting tides')
-    const tideRecord = await getTides()
-    if (isJsonRequest(req)) {
-        res.set('Content-Type', 'application/json')
-        res.send(JSON.stringify(tideRecord))
-    } else {
-        res.set({ 'content-type': 'text/plain' })
-        res.send(Sqrl.render(tideTemplate, { tideRecord }))
-    }
-}
-
-async function handleDefaultRoute(req: Request, res: ResponseBuilder) {
-    res.set({ 'content-type': 'text/plain' })
-    res.send('no such route, try another')
-}
-
 export async function handler(req: Request, res: ResponseBuilder) {
     try {
         await router.handleRequest(req, res)
     }
     catch (error: unknown) {
-        console.log(error)
+        console.error(error)
         handleError(res, error)
     }
-
-}
-
-function isJsonRequest(req: Request) {
-    return req.headers.get('Content-Type') === 'application/json'
 }
---
+++ hateoas-api/src/routes/handleGetTides.ts
@@ -0,0 +1,17 @@
+import { ResponseBuilder } from '@fermyon/spin-sdk'
+import * as Sqrl from 'squirrelly'
+import { isJsonRequest } from '../lib/isJsonRequest'
+import { getTides } from '../lib/getTides'
+import { tideTemplate } from '../templates/tides'
+
+export async function handleGetTides(req: Request, res: ResponseBuilder) {
+    console.log('getting tides')
+    const tideRecord = await getTides()
+    if (isJsonRequest(req)) {
+        res.set('Content-Type', 'application/json')
+        res.send(JSON.stringify(tideRecord))
+    } else {
+        res.set({ 'content-type': 'text/plain' })
+        res.send(Sqrl.render(tideTemplate, { tideRecord }))
+    }
+}
