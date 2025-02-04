402f6362afa0a75b0bc0b744e03a521c3bd3574f
markysoft <markharrop@yahoo.com>
2025-02-04 16:14:56 +0000

    add caching

--- hateoas-api/src/routes/handleGetBankHolidays.ts
+++ hateoas-api/src/routes/handleGetBankHolidays.ts
@@ -3,10 +3,12 @@ import * as Sqrl from 'squirrelly'
 import { isJsonRequest } from '../lib/isJsonRequest'
 import { getBankHolidays } from '../getBankHolidays'
 import { holidayTemplate } from '../templates/holiday-list'
+import { twentyFourHoursInSeconds } from '../config'
 
 export async function handleGetBankHolidays(req: Request, res: ResponseBuilder) {
     console.log('getting bank holidays')
     const holidays = await getBankHolidays()
+    res.set('Cache-Control', `public, max-age=${twentyFourHoursInSeconds}`)
     if (isJsonRequest(req)) {
         res.set('Content-Type', 'application/json')
         res.send(JSON.stringify(holidays))
--- hateoas-api/src/routes/handleGetTides.ts
+++ hateoas-api/src/routes/handleGetTides.ts
@@ -3,16 +3,17 @@ import * as Sqrl from 'squirrelly'
 import { isJsonRequest } from '../lib/isJsonRequest'
 import { getTides } from '../lib/getTides'
 import { tideTemplate } from '../templates/tides'
+import { twelveHoursInSeconds } from '../config'
 
 export async function handleGetTides(req: Request, res: ResponseBuilder) {
     console.log('getting tides')
     const tideRecord = await getTides()
+    res.set('Cache-Control', `public, max-age=${twelveHoursInSeconds}`)
     if (isJsonRequest(req)) {
         res.set('Content-Type', 'application/json')
         res.send(JSON.stringify(tideRecord))
     } else {
-        res.set({ 'content-type': 'text/plain' })
-        res.set({ 'Last-Modified': 'text/plain' })
+        res.set('Content-Type', 'text/html')
         res.send(Sqrl.render(tideTemplate, { tideRecord }))
     }
 }
